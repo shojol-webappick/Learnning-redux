2e75abddb6f99a2d7d2b6d6153e1052421ffb513
Shojol Islam <shojolislam.webappick@gmail.com>
2026-02-18 15:22:19 +0600

    Add default value for task prop in Task component

--- src/TaskItem.jsx
+++ src/TaskItem.jsx
@@ -1,4 +1,4 @@
-export default function Task({ task, updateTask, deleteTask }) {
+export default function Task({ task = {}, updateTask, deleteTask }) {
   const { id, value, completed } = task;
 
   return (
@@ -53,3 +53,4 @@ export default function Task({ task, updateTask, deleteTask }) {
     </div>
   );
 }
+
